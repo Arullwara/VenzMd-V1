944924b88a19aa69f95378f1db7419d8d415bb80
Arullwara <106657360+Arullwara@users.noreply.github.com>
2022-06-06 07:11:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6285748894171'] // Put your number here
+global.owner = ['6285822347348'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -27,12 +27,12 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.stiker_wait = 'Stiker sedang dibuat'
+global.stiker_wait = '*WAITT NGOPI DULUU*'
 global.packname = 'By'
-global.author = ' Nakata'
-global.email = 'valencianakata1447@gmail.com'
+global.author = '*ArullOfc*'
+global.email = 'sahrulwara62@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© NahBotz-MD V2'
+global.wm = '© ArullBotz-MD V2'
 global.media = 'https://telegra.ph/file/fb94b67e8562c0be33235.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
